eff75cfd38c30a2b8445bebf76c7b0702d3eca45
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2022-02-12 15:38:41 +0800

    feat(form skeleton): 添加表单骨架和避免回流和重绘

--- src/pages/application/components/application-form/components/form-anchor.tsx
+++ src/pages/application/components/application-form/components/form-anchor.tsx
@@ -1,26 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { Anchor } from 'antd';
+import { Collapse, Anchor } from 'antd';
 
 const { Link } = Anchor;
+const { Panel } = Collapse;
 
 const FormAnchor: React.FC = () => {
   const [targetOffset, setTargetOffset] = useState<number | undefined>(undefined);
+
   useEffect(() => {
     setTargetOffset(window.innerHeight / 2);
   }, []);
   return (
-    <Anchor targetOffset={targetOffset}>
-      <Link href="#moral-form" title="思想品德" />
-      <Link href="#practice-form" title="实践活动">
-        <Link href="#practice-form-result" title="实践成果"></Link>
-        <Link href="#practice-form-social" title="社会活动">
-          <Link href="#practice-form-social-cadre" title="校园职位"></Link>
-          <Link href="#practice-form-social-activity" title="校园活动"></Link>
-        </Link>
-        <Link href="#practice-form-competition" title="竞赛信息"></Link>
-      </Link>
-      <Link href="#academic-form" title="学术成果"></Link>
-    </Anchor>
+    <React.Fragment>
+      <Anchor
+        showInkInFixed
+        targetOffset={targetOffset}
+        style={{
+          backgroundColor: "#F0F2F5",
+          marginLeft: 16
+        }}>
+          <Link href="#moral-form" title="思想品德" />
+          <Link href="#practice-form" title="实践活动">
+            <Link href="#practice-form-result" title="实践成果"></Link>
+            <Link href="#practice-form-social" title="社会活动">
+              <Link href="#practice-form-social-cadre" title="校园职位"></Link>
+              <Link href="#practice-form-social-activity" title="校园活动"></Link>
+            </Link>
+            <Link href="#practice-form-competition" title="竞赛信息"></Link>
+          </Link>
+          <Link href="#academic-form" title="学术成果">
+            <Link href="#academic-form-scientific" title="科研项目"></Link>
+          </Link>
+      </Anchor>
+    </React.Fragment>
   );
 };
 
--- src/pages/application/components/application-form/components/practice-form/components/competition-form.tsx
+++ src/pages/application/components/application-form/components/practice-form/components/competition-form.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as C from '../../../config/practice.config';
 import { disabledFormCurrentDate } from '@/config/form';
-import { Card, FormItemProps, Form, DatePicker, Cascader, Input, InputNumber, Typography, Button, DatePickerProps, Alert } from 'antd';
+import { FormItemProps, Form, DatePicker, Cascader, Input, InputNumber, DatePickerProps } from 'antd';
 import moment from 'moment';
-import { PlusOutlined } from '@ant-design/icons';
+import FormListSkeleton from '../../form-list-skeleton';
 
 export interface PracticeCompetitionFormValue {
   level: [C.CompetitionScoreItem['type'], C.CompetitionLevelScoreItem['level']] | [];
@@ -27,7 +27,6 @@ interface IProps extends FormItemProps {
 
 const PracticeCompetitionForm: React.FC<IProps> = (props) => {
 
-  const { ...resetProps } = props;
   const competitionOptions = React.useMemo(() => C.competitionScoreList.map(item => ({
     label: item.title,
     value: item.type,
@@ -39,94 +38,67 @@ const PracticeCompetitionForm: React.FC<IProps> = (props) => {
   })), []);
 
   return (
-    <Card
+    <FormListSkeleton
+      name="competition"
+      itemTitle="竞赛"
       title="竞赛信息表"
-      id="practice-form-competition"
+      listId="practice-form-competition"
+      alertMessage="仅认可互联网+、挑战杯、中国研究生创新实践系列大赛、陕西省研究生创新成果展。研究生创新成果展与省赛同级"
     >
-      <Alert
-        message="仅认可互联网+、挑战杯、中国研究生创新实践系列大赛、陕西省研究生创新成果展。研究生创新成果展与省赛同级"
-        type="info"
-        showIcon
-      />
-      <Form.Item {...resetProps} noStyle>
-        <Form.List
-          name="competition"
-        >
-          {
-            (fields, { add, remove }) => {
-              return (
-                <React.Fragment>
-                  {
-                    fields.map(field => (
-                      <Card
-                        title={`竞赛 ${field.key + 1}`}
-                        type="inner"
-                      >
-                        <Form.Item
-                          label="竞赛类型"
-                          name={[field.name, "level"]}
-                        >
-                          <Cascader
-                            placeholder="竞赛类型"
-                            options={competitionOptions}
-                          />
-                        </Form.Item>
-                        <Form.Item
-                          label="赛事具名"
-                          name={[field.name, "name"]}
-                        >
-                          <Input />
-                        </Form.Item>
-                        <Form.Item
-                          label="获奖时间"
-                          name={[field.name, "time"]}
-                        >
-                          <DatePicker
-                            disabledDate={disabledFormCurrentDate}
-                          />
-                        </Form.Item>
-                        <Form.Item
-                          label="合作信息"
-                        >
-                          <Form.Item
-                            noStyle
-                            name={[field.name, "order"]}
-                          >
-                            <InputNumber
-                              addonBefore="个人贡献名次"
-                              addonAfter="名"
-                              min={1}
-                            />
-                          </Form.Item>
-                          <Form.Item
-                            name={[field.name, "partners"]}
-                            noStyle
-                          >
-                            <InputNumber
-                              addonBefore="团队人数"
-                              addonAfter="人"
-                              min={1}
-                            />
-                          </Form.Item>
-                        </Form.Item>
-                      </Card>
-                    ))
-                  }
-                  <Button
-                    type="dashed"
-                    onClick={() => add()}
-                    block
-                    icon={<PlusOutlined />}
-                  >
-                    添加比赛
-                    </Button>
-                </React.Fragment>
-              )
-            }
-          }
-        </Form.List>
-      </Form.Item>
-    </Card >
+      {
+        (field) => (
+          <React.Fragment>
+            <Form.Item
+              label="竞赛类型"
+              name={[field.name, "level"]}
+            >
+              <Cascader
+                placeholder="竞赛类型"
+                options={competitionOptions}
+              />
+            </Form.Item>
+            <Form.Item
+              label="赛事具名"
+              name={[field.name, "name"]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="获奖时间"
+              name={[field.name, "time"]}
+            >
+              <DatePicker
+                disabledDate={disabledFormCurrentDate}
+              />
+            </Form.Item>
+            <Form.Item
+              label="合作信息"
+            >
+              <Form.Item
+                noStyle
+                name={[field.name, "order"]}
+              >
+                <InputNumber
+                  addonBefore="个人贡献名次"
+                  addonAfter="名"
+                  min={1}
+                />
+              </Form.Item>
+              <Form.Item
+                name={[field.name, "partners"]}
+                noStyle
+              >
+                <InputNumber
+                  addonBefore="团队人数"
+                  addonAfter="人"
+                  min={1}
+                />
+              </Form.Item>
+            </Form.Item>
+          </React.Fragment>
+        )
+      }
+    </FormListSkeleton>
   );
 
 };
