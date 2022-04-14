23b4928b2e7496fd7711edc767abe43969107ad7
baiziyu-fe <baiziyu-fe@outlook.com>
2022-04-14 10:53:40 +0800

    fix(action): action

---
+++ dist/static/js/index-4d5ce0d6.js
@@ -0,0 +1,2 @@
+import{r as e,ay as t,e as r,Y as o}from"./vendor-8198df31.js";const a=()=>e.exports.createElement(t,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:e.exports.createElement(r,{type:"primary"},e.exports.createElement(o,{to:"/"},"Home"))});export{a as default};
+//# sourceMappingURL=index-4d5ce0d6.js.map
